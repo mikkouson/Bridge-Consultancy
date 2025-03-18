aaf394f86c8415b21dffa86fa5c99220436f54ff
Leo Mikko Uson <usonleomikko@gmail.com>
2025-03-18 13:25:41 +0800

    Replace EditCompanySheet with SheetModal for company creation in DataTableRowActions

--- app/(admin)/companies/components/data-table-row-actions.tsx
+++ app/(admin)/companies/components/data-table-row-actions.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { Row } from "@tanstack/react-table";
-import { EditCompanySheet } from "@/components/companies/edit-company-sheet";
 import { CompanySchemaType } from "@/app/types/companies.type";
 import { DeleteConfirmationDialog } from "@/components/delete-dialog";
 import { deleteCompanies } from "../actions";
+import { SheetModal } from "@/components/sheet-modal";
+import { EditCompany } from "@/components/companies/edit-company";
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
@@ -15,7 +16,14 @@ export function DataTableRowActions<TData extends CompanySchemaType>({
 }: DataTableRowActionsProps<TData>) {
   return (
     <div className="flex items-center justify-start ">
-      <EditCompanySheet data={row.original} />
+      <SheetModal
+        triggerLabel="Create Company"
+        title="Create Company"
+        description="Fill in the details to create a new company."
+        edit={true}
+      >
+        {(setOpen) => <EditCompany data={row.original} setOpen={setOpen} />}
+      </SheetModal>
       <DeleteConfirmationDialog
         onConfirm={async () => {
           await deleteCompanies((row.original as { id: number }).id);
