ed60ca7386a4fb28f9bfaacbd244d1466cae471f
Leo Mikko Uson <usonleomikko@gmail.com>
2025-05-18 16:38:14 +0800

    feat: add loading state and error handling to services page and useServices hook

--- app/(admin)/services/page.tsx
+++ app/(admin)/services/page.tsx
@@ -2,9 +2,12 @@
 import { columns } from "../../../components/services/columns";
 import { DataTable } from "../../../components/services/data-table";
 import { useServices } from "@/app/hooks/use-services";
+import Loader from "@/components/loader";
 
 export default function TaskPage() {
-  const { data } = useServices();
+  const { data, isLoading } = useServices();
+  if (isLoading) return <Loader />;
+
   return (
     <div className=" h-full flex-1 flex-col space-y-8 px-2 flex">
       <div className="flex items-center justify-between space-y-2">
